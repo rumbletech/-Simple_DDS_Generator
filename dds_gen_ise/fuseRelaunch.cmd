02fe19c15dfdd610b8ba44abd243d6f6486a4708
mrashada <mohamed.rashad-abdelghany@valeo.com>
2024-06-16 02:04:35 +0300

    Remove generated files from xilinx project directories.

--- dds_gen_ise/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "secureip" -o "C:/Main/-Simple_DDS_Generator/dds_gen_ise/tb_dds_gen_isim_beh.exe" -prj "C:/Main/-Simple_DDS_Generator/dds_gen_ise/tb_dds_gen_beh.prj" "DDS.tb_dds_gen" 
